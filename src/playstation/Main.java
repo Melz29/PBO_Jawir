e8e2bd347d4985fe2513ac470ad1885f5a230ab7
Lenovo <Lenovo@Ernest>
2025-07-29 13:19:46 +0700

    Ini adalah komit pertama - ernest

---
+++ src/playstation/Main.java
@@ -0,0 +1,20 @@
+package playstation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // 1. Buat objek untuk form pertama dengan nama variabel 'formBooking'
+    //BookingPod formBooking = new BookingPod();
+
+    // 2. Buat objek untuk form kedua dengan nama variabel 'formListPS5'
+    tampilan1B formListPS5 = new tampilan1B();
+
+    // 3. Tampilkan KEDUA form tersebut secara terpisah
+    //formBooking.setVisible(true);
+    //formListPS5.setVisible(true);
+        
+    }
+    
+}
